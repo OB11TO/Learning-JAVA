4a0b2a795e9b14e243dfa6c485877c7f390d7b8b
OB11TO <OB11TO61@yandex.ru>
2020-09-29 22:22:33 +0300

    added gu simbol obraz

--- computer_model/testPyQt.py
+++ computer_model/testPyQt.py
@@ -25,29 +25,6 @@ from tkinter.ttk import Combobox
 import tkinter as tk
 from tkinter import *
 
-x_0, y_0 = -2, 2 #наша ооблась определения
-
-'''
-print("Введите формулу для x")
-form_x = str(input())
-print("Введите формулу для y")
-form_y = str(input())
-
-print("Введите значения следующих параметров:\nДиаметр разбиения ")
-d = float(input())
-print("a ")
-a = float(input())
-print("b ")
-b = float(input())
-'''
-#N = int(4 / d)
-
-#print("Сохранять? (0/1)")
-#save = bool(int(input()))
-
-def formula(x, y):
-    return(eval(form_x), eval(form_y))
-
 def get_num(x, y):
     y = np.where(x < -2, 3, y)
     x = np.where(x < -2, -3, x)
@@ -66,34 +43,60 @@ def get_num(x, y):
     i = np.where(i < 0, 0, i)
     return i
 
+def formula(x, y):
+    return(eval(form_x), eval(form_y))
+
 def main():
-    graph = {}
-    pattern_x = np.ones((10, 10)) * np.array(range(1, 11)) * 0.1 * d - d / 20
-    pattern_y = pattern_x.copy().transpose()
-    n = 1
-    y_n = y_0
-    while(y_n > -y_0):
-        x_n = x_0
-        while(x_n < -x_0):
-            s = set(np.unique(get_num(*formula(x_n + pattern_x, y_n - pattern_y))).astype('int32'))
-            graph[n] = s
-            x_n += d
-            n += 1
+    if len(sys.argv) == 1:
+        global y_0, x_0, form_y, form_x, a, b, d, N, y_n, pattern_x, pattern_y, graph, n
+        global x_0
+        x_0, y_0 = -2, 2 #наша ооблась определения
+    
+
+        form_x = str(num1.get())
+        form_y = str(num2.get())
+        d = float(numyint.get())
+        a = float(numya.get())
+        b = float(numyb.get())
+
+        N = int(4 / d)
+
+
+        graph = {}
+        pattern_x = np.ones((10, 10)) * np.array(range(1, 11)) * 0.1 * d - d / 20
+        pattern_y = pattern_x.copy().transpose()
+        n = 1
+        y_n = y_0
+        while(y_n > -y_0):
+            x_n = x_0
+            while(x_n < -x_0):
+                s = set(np.unique(get_num(*formula(x_n + pattern_x, y_n - pattern_y))).astype('int32'))
+                graph[n] = s
+                x_n += d
+                n += 1
             y_n -= d
-'''
-if save:
-    cntr = 0
-    c = 1
-    f = open('test.txt', 'w')
-    f.write('a = {}, b = {}\n'.format(a, b))
-    for k, v in graph.items():
-        f.write(f'{k}: {v}\n')
-        cntr += len(v)
-        c += 1
-    f.close()
-    print("Количество рёбер: {}".format(cntr))
-    print("Количество вершин: {}".format(c))
-'''
+
+
+        print("Сохранять? (0/1)")
+        save = bool(int(input()))
+        if save:
+            cntr = 0
+            c = 1
+            f = open('test.txt', 'w')
+            f.write('a = {}, b = {}\n'.format(a, b))
+            for k, v in graph.items():
+                f.write(f'{k}: {v}\n')
+                cntr += len(v)
+                c += 1
+            f.close()
+            print("Количество рёбер: {}".format(cntr))
+            print("Количество вершин: {}".format(c))
+    else:
+        try:
+            time_all = np.float(sys.argv[1])
+        except:
+            raise Exception('Argument must be a number')
+
 # функция выхода из программы
 def quit():
     exit(0)
@@ -107,10 +110,13 @@ root.resizable(False, False)
 #Для уравнения
 lbl = Label(root, text = "f(x,y)= ")
 lbl.place(x = 0, y = 90, height = 30, width = 50)
+
 lbl1 = Label(root, text = "Формула ")
 lbl1.place(x = 0, y = 10, height = 30, width = 80)
+
 lbl2 = Label(root, text = "Действие ")
 lbl2.place(x = 400, y = 10, height = 30, width = 80)
+
 lblx = Label(root, text = "Область определения ")
 lblx.place(x = 0, y = 180, height = 30, width = 170)
 lblx1 = Label(root, text = "x ")
