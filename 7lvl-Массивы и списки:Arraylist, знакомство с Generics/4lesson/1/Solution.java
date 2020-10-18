33a8795685910f2faf1b96f9bc08644774721370
OB11TO <OB11TO61@yandex.ru>
2020-10-18 16:11:06 +0300

    Оптимизированны папки, добавлена задача

---
+++ 7lvl-Массивы и списки:Arraylist, знакомство с Generics/4lesson/1/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task07.task0702;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+Массив из строчек в обратном порядке
+1. Создать массив на 10 строк.
+2. Ввести с клавиатуры 8 строк и сохранить их в массив.
+3. Вывести содержимое всего массива (10 элементов) на экран в обратном порядке. Каждый элемент - с новой строки.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] mas = new String [10];
+        for(int i = 0; i < 8; i++ ){
+            mas[i] = reader.readLine();
+        }
+
+        for(int i = 9; i >= 0; i--){
+            System.out.println(mas[i]);
+        }
+    }
+}
+
+/*  Ввод
+1
+2
+3
+4
+5
+6
+7
+8
+null
+    Вывод
+null
+8
+7
+6
+5
+4
+3
+2
+1
+* */
