f8bf82a724f4e3b77ca009f8e6d475659c814b45
OB11TO <OB11TO61@yandex.ru>
2020-11-01 16:00:31 +0300

    +

---
+++ 4lvl-Ветвления и циклы/4lesson/5/Sol.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task04.task0427;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+Описываем числа
+Пример для числа 100:
+четное трехзначное число
+
+Пример для числа 51:
+нечетное двузначное число
+
+Требования:
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+        if (number > 0 && number < 10 && number % 2 == 0) {
+            System.out.println("четное однозначное число");
+        } else if (number < 10 && number % 2 != 0) {
+            System.out.println("нечетное однозначное число");
+        } else if (number > 9 && number < 100 && number % 2 == 0) {
+            System.out.println("четное двузначное число");
+        } else if (number > 9 && number < 100 && number % 2 != 0) {
+            System.out.println("нечетное двузначное число");
+        } else if (number > 99 && number < 1000 && number % 2 == 0) {
+            System.out.println("четное трехзначное число");
+        } else if (number > 99 && number < 1000 && number % 2 != 0) {
+            System.out.println("нечетное трехзначное число");
+        }
+    }
+}
