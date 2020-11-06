f7f1a9e83985bce08ae59e911052997d3913eb60
OB11TO <OB11TO61@yandex.ru>
2020-11-06 21:11:53 +0300

    +

---
+++ 5lvl-class-object/5lesson/2/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task05.task0502;
+
+/*
+Реализовать метод fight
+
+Реализуй метод boolean fight(Cat anotherCat):
+продумай механизм драки котов в зависимости от их веса, возраста и силы.
+Сравни каждый критерий по отдельности, чтобы победителем был тот, у кого были лучшие показатели по большинству критериев.
+Метод должен определять, выиграли ли мы (this) бой или нет, т.е. возвращать true, если выиграли и false - если проиграли.
+Если ничья и никто не выиграл, возвращаем false
+*/
+
+public class Cat {
+    public int age;
+    public int weight;
+    public int strength;
+
+    public Cat() {
+    }
+
+    public boolean fight(Cat anotherCat) {
+        int ageScore = Integer.compare(this.age, anotherCat.age);
+        int weightScore = Integer.compare(this.weight, anotherCat.weight);
+        int strengthScore = Integer.compare(this.strength, anotherCat.strength);
+
+        int score = ageScore + weightScore + strengthScore;
+        return score > 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
