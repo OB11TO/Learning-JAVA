bf80a713136751a603dbd5ac9df420e68b010e5a
OB11TO <OB11TO61@yandex.ru>
2020-11-15 15:26:55 +0300

    +

---
+++ 8lvl-Коллекции:LinkedList, HashSet, HashMap, Data/6lesson/1/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task08.task0807;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/*
+LinkedList и ArrayList
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        //напишите тут ваш код
+        ArrayList<String> list = new ArrayList<>();
+        return list;
+
+    }
+
+    public static Object createLinkedList() {
+        //напишите тут ваш код
+        LinkedList<String> list = new LinkedList<>();
+        return list;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+
+public class Solution {
+    public static Object createArrayList() {
+        return new ArrayList();
+    }
+
+    public static Object createLinkedList() {
+        return new LinkedList();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+*/
