f499c7fa62c947cc0f01f81decc8cf458e640fc8
OB11TO <OB11TO61@yandex.ru>
2020-11-25 14:28:02 +0300

    +

--- 9lvl-Исключения try,catch,throws/12lesson/10/Solution.java
+++ 9lvl-Исключения try,catch,throws/12lesson/10/Solution.java
@@ -1,17 +1,80 @@
-int i = 5;
-float f = 444.23f;
-String s = "17";
-Object o = f;                       //o хранит объект типа Float
+package com.javarush.task.task09.task0930;
 
-if (o instanceof  Integer)
-{
-    Integer i2 = (Integer) o;
-}
-else if (o instanceof  Float)
-{
-    Float f2 = (Float) o;            //отработает именно этот if
-}
-else if (o instanceof  String)
-{
-    String s2 = (String) o;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/*
+Задача по алгоритмам Ӏ Java Syntax: 9 уровень, 11 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        String line = reader.readLine();
+        while (!line.isEmpty()) {
+            list.add(line);
+            line = reader.readLine();
+        }
+
+        String[] array = list.toArray(new String[0]);
+        sort(array);
+
+        for (String x : array) {
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (isNumber(array[i]) && isNumber(array[j])) {
+                    if (Integer.parseInt(array[i]) < Integer.parseInt(array[j])) {
+                        String temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                } else if (!isNumber(array[i]) && !isNumber(array[j])) {
+                    if (isGreaterThan(array[i], array[j])) {
+                        String temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+
+    public static boolean isNumber(String text) {
+        if (text.length() == 0) {
+            return false;
+        }
+
+        char[] chars = text.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char character = chars[i];
+
+            // есть '-' внутри строки
+            if (i != 0 && character == '-') {
+                return false;
+            }
+
+            // не цифра и не начинается с '-'
+            if (!Character.isDigit(character) && character != '-') {
+                return false;
+            }
+
+            // одиночный '-'
+            if (chars.length == 1 && character == '-') {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
