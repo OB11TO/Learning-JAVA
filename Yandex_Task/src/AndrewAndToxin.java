078f7270205604395906d457c28bc7160bb081e6
OB11TO <OB11TO61@yandex.ru>
2021-03-31 19:07:19 +0300

    new mod

--- Yandex_Task/src/AndrewAndToxin.java
+++ Yandex_Task/src/AndrewAndToxin.java
@@ -9,24 +9,49 @@ public class AndrewAndToxin {
         int n = scanner.nextInt();
 
 
-
         System.out.println("Введите количество литров аi: ");
         int[] reserv = new int[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             reserv[i] = scanner.nextInt();
         }
-
+        
+        int l = 0;
         int max = reserv[0];
         int min = reserv[0];
 
-        for(int i = 0; i < n -1 ; i++) { //упорядочный
+        for(int i = 1; i < n; i++){ //min max
+            if(reserv[i] > max){
+                max = reserv[i];
+                l = i;
+            }
+            if(reserv[i] < min){
+                min = reserv[i];
+            }
+        }
+
+        for (int i = 0; i < n - 1; i++) { //упорядочный
             if (reserv[i] > reserv[i + 1]) {
                 System.out.println("-1");
                 break;
             }
+            else{
+                int[] mass = new int[l];
+                for(int j = 0; j < n; j++){
+                    if(reserv[j] != max){
+                        mass[j] = reserv[j];
+                    }
+                    else {
+                        leet(min,max,mass);
+                        break;
+                    }
+
+                }
+            }
         }
 
-        for(int i = 1; i < n; i++){ //min max
+        //
+
+      /*  for(int i = 1; i < n; i++){ //min max
             if(reserv[i] > max){
                 max = reserv[i];
             }
@@ -35,11 +60,26 @@ public class AndrewAndToxin {
             }
         }
 
+        int s = max - min;
+
+        if(s % 2 != 0){
+            System.out.println("2222222222 ");
+
+        }
+        else{
+            int k = 1;
+            leet(k);
+        }
 
 
 
-//        System.out.println(min + " " + max);
+    }
 
-        System.out.println(Arrays.toString(reserv));
+    public static void leet(int k){
+
+    }*/
+    }
+    public static void leet(int min, int max, int[] mass){
+        System.out.println(max + " " + min+ " "+ Arrays.toString(mass));
     }
 }
