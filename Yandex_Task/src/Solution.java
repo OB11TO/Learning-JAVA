0acdc6d3a562753da0c270b819715a3d15c9b9df
OB11TO <OB11TO61@yandex.ru>
2021-04-04 21:08:41 +0300

    solution getCurrencyInstance

--- Yandex_Task/src/Solution.java
+++ Yandex_Task/src/Solution.java
@@ -1,25 +1,27 @@
-import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Random;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
-    private int x, y;
-
-    Solution(int x, int y) {
-        this.y = y;
-        System.out.println(x + " " + y);
-    }
 
     public static void main(String[] args) {
-        System.out.println(findDay(8, 5, 2015));
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
 
-    }
+        Locale INDIA = new Locale("en", "IN");//Creates a new Locale with English as the language and India as teh country
 
-    public static String findDay(int month, int day, int year) {
-        LocalDate c = LocalDate.of(year, month, day);
 
-            String s = String.valueOf(c.getDayOfWeek());
-            return s;
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(INDIA).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
     }
 }
