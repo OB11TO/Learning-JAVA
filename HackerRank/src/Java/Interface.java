8c0ff16555ec3442b14c4caa1fd1fc8944897c2e
OB11TO <OB11TO61@yandex.ru>
2021-04-14 22:31:01 +0300

    была реализованна функция divisor_sum

--- HackerRank/src/Java/Interface.java
+++ HackerRank/src/Java/Interface.java
@@ -20,7 +20,13 @@ class MyCalculator implements AdvancedArithmetic {
 
     @Override
     public int divisor_sum(int n) {
-
+        int sum = 0;
+        for(int i = 1; i<=n; i++){  // 1 < 6    6 /1
+            if((n % i) == 0){
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
 public class Interface{
