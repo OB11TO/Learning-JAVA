e0c9eac354452ab2f595f45c4646f14e327bc2e0
OB11TO <OB11TO61@yandex.ru>
2021-04-20 21:27:11 +0300

    создан класс Main

--- CoursesTask/src/Hotel/Main.java
+++ CoursesTask/src/Hotel/Main.java
@@ -1,6 +1,13 @@
 package Hotel;
 
+import Hotel.entity.Bill;
 import Hotel.entity.Client;
+import Hotel.entity.Hotel;
+import Hotel.entity.Room;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * В данном домашнем задании нужно написатьсистему бронировния номеров в отеле.*
@@ -12,8 +19,8 @@ import Hotel.entity.Client;
  * Room будет содеражть колличество человек, которых можно разместить, стоимость этого номера и свободен ли этот номер
  *
  * Сценарии:
- * Нужно создать нескольких клиентов с счетами и внести им первоначальную сумму. Так же нужно создать
- * несколько отелей, с разными наборами номеров (В списке внутри сущности Hotel)
+ * Нужно создать нескольких клиентов с счетами и внести им первоначальную сумму.
+ * Так же нужно создать несколько отелей, с разными наборами номеров (В списке внутри сущности Hotel)
  * Клиент должен попробовать забронировать номер, для этого он так же указывает
  * сколько человек в номере ему нужно разместить. После успешного бронирования списывать сумму со счета клиента.
  *
@@ -25,6 +32,24 @@ import Hotel.entity.Client;
  */
 public class Main {
     public static void main(String[] args) {
+        Bill firstBill = new Bill(1200);
+        Client firstClient = new Client("Bran", "Starck", "+7939433213", firstBill );
+
+        Bill secondBill = new Bill(92000);
+        Client secondClient = new Client("Artem", "Man","+79894256781",secondBill);
+
+        List<Room>  roomGrand = Arrays.asList(new Room(1, 350, true),
+                new Room(2, 500, true),
+                new Room(2, 450, false));
+        Hotel hotelGrand = new Hotel("Grand", roomGrand);
+
+        List<Room> roomRoyal = Arrays.asList(new Room(2,950,true),
+                new Room(4,1500,false),
+                new Room(3,1300,true));
+        Hotel hotelRoyal = new Hotel("Royal", roomRoyal);
+
+
+
 
     }
 
