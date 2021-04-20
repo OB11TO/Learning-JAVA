43f4bf74cef2c945b92a2e14297d8082c3807647
OB11TO <OB11TO61@yandex.ru>
2021-04-20 21:27:44 +0300

    пофикшена ошибка String number

--- CoursesTask/src/Hotel/entity/Client.java
+++ CoursesTask/src/Hotel/entity/Client.java
@@ -3,16 +3,15 @@ package Hotel.entity;
 public class Client {
     private String firstName;
     private String lastName;
-    private int number;
+    private String  number;
+    private Bill bill;
 
-    public Client(String firstName, String lastName, int number, Bill bill) {
+    public Client(String firstName, String lastName, String number, Bill bill) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.number = number;
         this.bill = bill;
     }
-
-    private  Bill bill;
 
     public String getFirstName() {
         return firstName;
@@ -30,11 +29,11 @@ public class Client {
         this.lastName = lastName;
     }
 
-    public int getNumber() {
+    public String getNumber() {
         return number;
     }
 
-    public void setNumber(int number) {
+    public void setNumber(String number) {
         this.number = number;
     }
 
