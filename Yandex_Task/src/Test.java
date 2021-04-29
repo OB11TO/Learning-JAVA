e957192be3797f0e679046876b1c6efadc3c7ee9
OB11TO <OB11TO61@yandex.ru>
2021-04-29 18:04:27 +0300

    test - с полиморфизмом

--- Yandex_Task/src/Test.java
+++ Yandex_Task/src/Test.java
@@ -1,15 +1,23 @@
-class Vehicle{
-    public static void  kmToMiles(int km){
-        System.out.println("Внутри родительского класса/статического метода");
-    } }
-
-class Car extends Vehicle{
-    public static void  kmToMils(int km){
-        System.out.println("Внутри дочернего класса/статического метода ");
-    } }
-
-public class Test{
-    public static void main(String args[]){
-        Vehicle v = new Car();
-        ((Car) v).kmToMils(10);
-    }}
+public class Test {
+    public void perform() {
+        this.perform(1);
+        System.out.print("1");
+    }
+    public void perform(int val) {
+        System.out.print(val);
+    }
+    public static void main(String[] args) {
+        Test c = new SomeClass2();
+        c.perform(4);
+    }
+}
+class SomeClass2 extends Test {
+    public void perform() {
+        super.perform();
+        System.out.print("3");
+    }
+    public void perform(int val) {
+        super.perform();
+        System.out.print(val);
+    }
+}
