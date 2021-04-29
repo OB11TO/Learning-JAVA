2a0905c7faab38eb5eb7d0986e5c09492c6534c4
OB11TO <OB11TO61@yandex.ru>
2021-04-29 18:38:09 +0300

    test - переопределением hashcode c правильной реализацией

--- Yandex_Task/src/Test.java
+++ Yandex_Task/src/Test.java
@@ -15,6 +15,13 @@ public class Test {
 class BlackBox {
     int varA;
     int varB;
+    private boolean booleanValue = true;
+    private char charValue = 'd';
+    private String stringValue = "TestClass";
+    private long longValue = 34829245849498300l;
+    private float floatValue = 345832400.93f;
+    private double doubleValue = 98584292348454.9834;
+    private byte[] arrayValue = {1, 2, 3};
 
     BlackBox(int varA, int varB){
         this.varA = varA;
@@ -31,7 +38,19 @@ class BlackBox {
 
     @Override
     public int hashCode() {
-        return Objects.hash(varA, varB);
+        int result = 17;
+
+        result = 37 * result + ( booleanValue ? 1 : 0 );
+        result = 37 * result + (int) charValue;
+        result = 37 * result + (stringValue == null ? 0 : stringValue.hashCode());
+        result = 37 * result + (int)(longValue - (longValue >>> 32));
+        result = 37 * result + Float.floatToIntBits(floatValue);
+        long longBits = Double.doubleToLongBits(doubleValue);
+        result = 37 * result + (int)(longBits - (longBits >>> 32));
+        for( byte b : arrayValue )
+            result = 37 * result + (int) b;
+
+        return result;
     }
 }
 
